4fb4f88fd725b446ef28311ebae0f80c34d5328a
bouanani-soufiane <soufian.bouanani2013@gmail.com>
2025-02-18 15:51:55 +0100

    Initialize moneyInTransaction and validate insertMoney in SnackMachine

--- snack-machine/src/main/java/com/progressoft/induction/SnackMachine.java
+++ snack-machine/src/main/java/com/progressoft/induction/SnackMachine.java
@@ -4,20 +4,34 @@ public class SnackMachine {
     static final Object DEFAULT_QUANTITY = 10;
 
     private Money moneyInside;
+    private Money moneyInTransaction;
+
 
     public SnackMachine() {
         this.moneyInside = Money.ZERO;
+        this.moneyInTransaction = Money.ZERO;
+
     }
 
+    public void insertMoney(Money money) {
+        if (money == null) {
+            throw new IllegalArgumentException("Money cannot be null");
+        }
+        if (!(money.equals(Money.QUARTER_DINAR) ||
+                money.equals(Money.HALF_DINAR) ||
+                money.equals(Money.DINAR) ||
+                money.equals(Money.FIVE_DINAR) ||
+                money.equals(Money.TEN_DINAR))) {
+            throw new IllegalArgumentException("Unsupported money unit");
+        }
+        moneyInTransaction = moneyInTransaction.add(money);
+    }
     public Money moneyInside () {
         return moneyInside;
     }
 
-    public void insertMoney ( Object quarterDinar ) {
-    }
-
-    public boolean moneyInTransaction () {
-        return false;
+    public Money moneyInTransaction() {
+        return moneyInTransaction;
     }
 
     public Object chewingGums () {
